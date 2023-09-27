2bd059c2bdc1c861925bf1a0f15da0ab0e7232a2
Yuri Cruz <yuricruzf@gmail.com>
2023-09-27 11:07:31 -0300

    Adding images from public folder

--- aula01/a1.3/app/page.js
+++ aula01/a1.3/app/page.js
@@ -1,5 +1,10 @@
 const index = () => {
-  return <h1>Welcome to NextJs</h1>;
+  return (
+    <>
+      <h1>Welcome to NextJs</h1>
+      <img src="/images/avatar.png" alt="" />
+    </>
+  );
 };
 
 export default index;
