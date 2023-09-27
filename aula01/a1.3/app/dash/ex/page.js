f20b336c5b07ecf0b7b77929ad82f2e3bd703270
Yuri Cruz <yuricruzf@gmail.com>
2023-09-27 02:27:00 -0300

    Starting project, initial example files

---
+++ aula01/a1.3/app/dash/ex/page.js
@@ -0,0 +1,5 @@
+const page = () => {
+  return <h1>Exemplo de subpáginas</h1>;
+};
+
+export default page;
---
+++ aula01/a1.3/app/dash/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+const page = () => {
+  return (
+    <h1 onClick={() => console.log("CLicado")}>
+      {/* <h1> */}
+      Exemplo de criação de página no novo modelo de app
+    </h1>
+  );
+};
+
+export default page;
